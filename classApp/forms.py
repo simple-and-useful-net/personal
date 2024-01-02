988bf39e2e46b44cb64eaee8b5daa5567356f3ee
simple-and-useful-net <kobayashi.kenji.jp@gmail.com>
2024-01-02 23:03:00 +0900

    test21

---
+++ classApp/forms.py
@@ -0,0 +1,188 @@
+
+from django import forms
+from .models import PersonalDataModel
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+
+class PersonalDataForm(forms.ModelForm):
+
+    LABELS={
+            "id":       "ID",
+            "name":     "名前",
+            "tel":      "電話番号",
+            "email":    "メール",
+            "sex":      "性別",
+            "prefecture":"都道府県",
+            "hobby":    "趣味",
+            "food":     "食べ物",
+    }
+
+    SEX_CHOICES = [
+        ('男', '男'),
+        ('女', '女'),
+    ]
+    PREFECT_CHOICES = [
+        ('北海道', '北海道'),
+        ('東京都', '東京都'),
+        ('大阪府', '大阪府'),
+    ]
+    HOBBY_CHOICES = [
+        ('音楽', '音楽'),
+        ('映画', '映画'),
+        ('アウトドア', 'アウトドア'),
+    ]
+    FOOD_CHOICES = [
+        ('ラーメン', 'ラーメン'),
+        ('寿司', '寿司'),
+        ('カレー', 'カレー'),
+    ]
+
+    # 性別、都道府県は複数選択が不可
+    sex = forms.ChoiceField(choices=SEX_CHOICES, initial="男", 
+                            widget=forms.RadioSelect())
+    prefecture = forms.ChoiceField(choices=PREFECT_CHOICES, initial="東京都")
+
+    # 趣味、食べ物は複数選択が可能
+    hobby = forms.MultipleChoiceField(choices=HOBBY_CHOICES, 
+                                      widget=forms.CheckboxSelectMultiple() )
+    food  = forms.MultipleChoiceField(choices=FOOD_CHOICES )
+
+    class Meta:
+        model = PersonalDataModel
+        fields = "__all__"
+        # fields = ["name", "email", "sex", "prefecture", "hobby","food"]
+
+        # widgets = {
+        #     'sex':      forms.RadioSelect(),
+        # }
+        # フォームページに出力される名称
+        # labels={
+        #     "name":"名前",
+        #     "sex":"性別",
+        #     "prefecture":"都道府県",
+        # }
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # すべてのフィールドを指定
+        for field_name, field in self.fields.items():
+            field.required = False  # フィールドのrequired属性をFalseに設定
+            field.label = PersonalDataForm.LABELS[field_name]
+
+            # ラジオボタン、チェックボックスは
+            # .form-check, .form-check-input, .form-check-label             
+            if not field_name in ["sex","hobby"]:
+                field.widget.attrs['class'] = 'form-control'
+
+        # フィールドを指定して設定
+        self.fields["name"].required = True
+        self.fields['name'].widget.attrs['placeholder'] = '名前を入力してください'
+
+    """
+    # モデルに保存
+    def save(self, commit=True):
+
+        instance = super(PersonalDataForm, self).save(commit=False)
+        
+        # フォームデータを変更
+        # instance.memo = instance.memo + 'New Value'  # フィールド名と新しい値を指定
+
+        # instance.categories.set(self.cleaned_data['categories'])
+        # 選択されたカテゴリをモデルに関連付ける
+        hobby10 = self.cleaned_data.get('hobby10')
+
+        if commit:
+            instance.save()
+        return instance
+
+
+
+    # 入力領域の個別チェックを行う場合はここでチェックする
+    def clean_hobby10(self):
+        data = self.cleaned_data.get('hobby10')
+        return data        
+    
+
+    # すべての入力項目のチェックをしたい場合に使用
+    # return時には「return self.cleaned_data」を返却が必要
+    def clean( self ):
+        # self.cleaned_data["hobby"]="abcdef"
+        data1 = self.cleaned_data.get('hobby')
+        data2 = self.cleaned_data.get('food')
+        print(data1, data2)
+
+        return self.cleaned_data
+    """
+
+
+
+
+
+
+# hobby     = TestField(choices=HOBBY_CHOICES, widget=forms.CheckboxSelectMultiple(), label="Alphanumeric Field")
+# hobby2      = MultiEmailField(widget=forms.CharField(), label='趣味ABC')
+
+class MultiEmailField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+
+        # self.choices = kwargs.pop('choices')
+        super().__init__(*args, **kwargs)
+        pass
+
+    def to_python(self, value):
+        """Normalize data to a list of strings."""
+        # Return an empty list if no input was given.
+        # if not value:
+        #     return []
+        # return value.split(",")
+        return "abcd"
+    
+    def validate(self, value):
+        """Check if value consists only of valid emails."""
+        # Use the parent's handling of required fields, etc.
+        super().validate(value)
+        for email in value:
+            validate_email(email)
+
+
+class TestField(forms.MultipleChoiceField):
+
+    def __init__(self, min_length=None, *args, **kwargs):
+        self.min_length = min_length
+        super().__init__(*args, **kwargs)    
+
+    def to_python(self, value):
+        print("to_python in ", value)
+        """Normalize data to a list of strings."""
+
+        # Return an empty list if no input was given.
+        # if not value:
+        #     return []
+        # return value.split(",")
+        return value
+
+    def validate(self, value):
+        print("validate in ", value)
+        super().validate(value)
+
+        # if not value.isalnum():
+        #     raise ValidationError("This field must contain only alphanumeric characters.")
+
+        # if self.min_length is not None and len(value) < self.min_length:
+        #     raise ValidationError(f"This field must be at least {self.min_length} characters long.")
+
+    def clean(self, value):
+        print("clean in ", value)
+        return str(value)
+        # if not value:
+        #     raise forms.ValidationError('Enter at least one e-mail address.')
+        # emails = value.split(',')
+        # for email in emails:
+        #     if not is_valid_email(email):
+        #         raise forms.ValidationError('%s is not a valid e-mail address.' % email)
+        # return emails
+
+        
---
+++ classApp/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render
+from django. shortcuts import render, redirect 
+from django. views. generic import ListView, DetailView, CreateView, DeleteView, UpdateView 
+from django. urls import reverse_lazy 
+from django. contrib. auth. models import User 
+from django. db import IntegrityError
+from django.core.exceptions import ValidationError
+from django. contrib. auth import authenticate, login ,logout 
+
+from .models import PersonalDataModel 
+from .forms import  PersonalDataForm 
+
+def make_context( md, ctx ):
+    
+    # ラベル名の取得
+    labels= PersonalDataForm.LABELS
+
+    # フィールドの取得
+    fields = md._meta.get_fields()
+
+    # テンプレートファイルの引数
+    obj =[]
+
+    for field in fields:
+        # フィード情報の取得
+        field_name = field.name
+        field_value = getattr( ctx["object"], field_name)
+        print( f"{field_name}= {field_value}" )
+
+        # フィード名からラベル名に変換
+        lbl_name = ""
+        if field_name in labels:       
+            lbl_name = labels[field_name]
+
+        # データ型がlistなら、漢字カンマ区切に変換
+        value = field_value
+        if type(value) is list:
+            value = "、".join(value)
+            
+        obj.append( {"name": lbl_name, "value":value})
+    
+    
+    ctx["object2"] = obj
+    return ctx
+
+
+# テンプレートのファイル名は、モデル名の小文字_???
+# ???_list.html
+# ???_detail.html
+# ???_confirm_delete.html
+# ???_form.html
+
+class PersonalList( ListView ):
+    model = PersonalDataModel 
+
+class PersonalDetail( DetailView ):
+    model = PersonalDataModel 
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx =make_context( PersonalDataModel, ctx )
+        return ctx
+
+class PersonalDelete( DeleteView ): 
+    model = PersonalDataModel
+    success_url = reverse_lazy('list_url')
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx =make_context( PersonalDataModel, ctx )
+        return ctx
+
+
+class PersonalCreate(CreateView):
+    model = PersonalDataModel 
+    form_class = PersonalDataForm
+    success_url = reverse_lazy('list_url')
+    extra_context = {'title': '登録'}
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['label_suffix'] = ''
+        return kwargs    
+
+    # def get_context_data(self, **kwargs):
+    #     ctx = super().get_context_data(**kwargs)
+    #     ctx["title"] = "登録"
+    #     return ctx
+
+class PersonalUpdate(UpdateView):
+    model = PersonalDataModel 
+    form_class = PersonalDataForm
+    success_url = reverse_lazy('list_url')
+    extra_context = {'title': '修正'}
+
+    # def get_context_data(self, **kwargs):
+    #     ctx = super().get_context_data(**kwargs)
+    #     ctx["title"] = "修正"
+    #     return ctx
+
+
+
+
+
